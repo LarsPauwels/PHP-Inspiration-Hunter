be96f0bcf0a21eb0f2540467d275ee2509b32c85
Nicolas <nicolas.gomez.colchero@gmail.com>
2019-04-04 21:43:57 +0200

    Login added works when password in hashed

--- login.php
+++ login.php
@@ -1,4 +1,25 @@
-<!DOCTYPE html>
+<?php
+	if( !empty($_POST) ) {
+			//Connection
+			$conn = new PDO("mysql:host=localhost;dbname=pl2", "root", "root");
+			//Get value out of POST
+			$email = $_POST['email'];
+			$password = $_POST['password'];
+
+			//Query
+			$statement = $conn->prepare("select * from users where email = :email");
+			//Placeholder for SQL-injection
+			$statement->bindParam(":email", $email);
+			$statement->execute();
+			$user = $statement->fetch(PDO::FETCH_ASSOC);
+	
+	//Checks if password matches the hash from database		
+	if( password_verify($password, $user['password'] ) ) {
+			//Yes? -> Go to index.php page
+			header('location: index.php');
+		}
+	}
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,25 +28,34 @@
     <title>LogIn</title>
 </head>
 <body>
+	<div>
+		<div>
+			<form action="" method="post">
+				<h2>Sign In</h2>
+				<div>
+					<label for="Email">Email</label>
+					<input type="text" name="email">
+				</div>
 
-    <div class="form__field">
-		<label for="Email">Email</label>
-		<input type="text" name="email">
-	</div>
+				<div>
+					<label for="Password">Password</label>
+					<input type="password" name="password">
+				</div>
 
-	<div class="form__field">
-		<label for="Password">Password</label>
-		<input type="password" name="password">
-	</div>
+				<div class="form__field">
+					<input type="submit" value="Sign in">	
+					<input type="checkbox" id="rememberMe"> 
+					<label for="rememberMe">Remember me</label>
+				</div>
 
-    <div class="form__field">
-		<input type="submit" value="Sign in" class="btn btn--primary">	
-		<input type="checkbox" id="rememberMe"><label for="rememberMe" class="label__inline">Remember me</label>
-	</div>
+				<br>
 
-    <div>
-		<p>No account yet?<a href="register.php">Sign up here</a></p>
+				<div>
+					<p>No account yet?<a href="register.php">Sign up here</a></p>
+				</div>
+			</form>
+
+		</div>
 	</div>
-    
 </body>
 </html>
