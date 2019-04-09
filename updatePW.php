01d578095f09cf51aca6e74bcbc9a975e5607887
Bram Theunis <bramtheunis1997@gmail.com>
2019-04-09 13:24:44 +0200

    Updated html form for more clarity for the user

--- updatePW.php
+++ updatePW.php
@@ -12,13 +12,18 @@
             <form action="" method="POST">
                 <h2>Update password</h2>
 
+				<div>
+					<label for="currentPassword">Current password</label>
+					<input type="password" name="currentPassword" id="currentPassword">
+				</div>
+
                 <div>
-					<label for="password">Password</label>
+					<label for="password">New password</label>
 					<input type="password" name="password" id="password">
 				</div>
 
                 <div>
-					<label for="confirmPassword">Confirm Password</label>
+					<label for="confirmPassword">Confirm new password</label>
 					<input type="password" name="confirmPassword" id="confirmPassword">
 				</div>
 
