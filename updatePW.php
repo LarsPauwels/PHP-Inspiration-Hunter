487e558f9acf954cd92d557eb8f36caadfbe3e63
Bram Theunis <bramtheunis1997@gmail.com>
2019-04-09 13:20:19 +0200

    Added basic html form to update password

---
+++ updatePW.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div>
+            <form action="" method="POST">
+                <h2>Update password</h2>
+
+                <div>
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password">
+				</div>
+
+                <div>
+					<label for="confirmPassword">Confirm Password</label>
+					<input type="password" name="confirmPassword" id="confirmPassword">
+				</div>
+
+                <div class="form__field">
+					<input type="submit" value="Update password">	
+				</div>
+
+            </form>
+        </div>
+    </div>
+</body>
+</html>
